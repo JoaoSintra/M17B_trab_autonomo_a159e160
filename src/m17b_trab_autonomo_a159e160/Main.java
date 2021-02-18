972f989b63dbaddc582b5d851038c06a5214bc36
JoaoSintra <sintr@JoaoSintra>
2021-02-18 12:18:26 +0000

    Versão Com JavaDoc

--- src/m17b_trab_autonomo_a159e160/Main.java
+++ src/m17b_trab_autonomo_a159e160/Main.java
@@ -1,8 +1,18 @@
-
 package m17b_trab_autonomo_a159e160;
 
-public class Main {
+/**
+ *
+ * @author João Sintra
+ * @version 2.0, 18.fev.2021
+ * 
+ */
 
+
+public class Main {
+    /**
+     * Não são usados parâmetros em main().
+     * @param args não se aplica.
+     */
     public static void main(String[] args) {
         Ponto2D p1 = new Ponto2D(5,7);
         Ponto2D p2 = new Ponto2D(2,8);
--- src/m17b_trab_autonomo_a159e160/Ponto2D.java
+++ src/m17b_trab_autonomo_a159e160/Ponto2D.java
@@ -1,23 +1,76 @@
 package m17b_trab_autonomo_a159e160;
 
+/**
+ * Classe Ponto2D, para instanciar objectos-coordenadas X e Y
+ * @version 2.0, 18.fev.2021
+ * @author João Sintra
+ */
 public class Ponto2D {
+    /** coordenada X */
     private int x;
+    /** coordenada Y */
     private int y;
+    //--------------------------------------------------------------------------
+    /**
+    * Construtor da classe parametrizado com todos os atributos.
+    * @param x coordenada x
+    * @param y coordenada y
+    */
+    public Ponto2D(int x, int y){
+        this.x = x;
+        this.y = y;        
+    }
+    //--------------------------------------------------------------------------
+    /**
+    * Construtor sem parâmetros, que inicializa os atributos
+    * com valores predefinidos conhecidos.
+    */
     
     public Ponto2D(){
         x = 0;
         y = 0;        
     }
-    
-    public Ponto2D(int x, int y){
-        this.x = x;
-        this.y = y;        
+    //--------------------------------------------------------------------------
+    /**
+    * @return valor da coordenada X
+    */
+    public int getX() {
+        return x;
     }
+    //--------------------------------------------------------------------------
+    /**
+    * @param x para definir a coordenada x
+    */
     
+    public void setX(int x) {
+        this.x = x;
+    }
+    //--------------------------------------------------------------------------
+    /**
+    * @return valor da coordenada Y
+    */
+    public int getY() {
+        return y;
+    }
+    //--------------------------------------------------------------------------
+    /**
+    * @param y para definir a coordenada y
+    */
+    public void setY(int y) {
+        this.y = y;
+    }
+    //--------------------------------------------------------------------------
+     /**
+    * @return a distancia de um objeto da classe Ponto2D a outro objeto da classe Ponto2D.
+    */
     public double distancia(Ponto2D a){
         return Math.abs(Math.sqrt(Math.pow((x-a.x),2)+Math.pow((y-a.y),2)));
     }
-
+    //--------------------------------------------------------------------------
+    /**
+    * 
+    * @return uma string composta pelos atributos da classe.
+    */
     @Override
     public String toString() {
         return "Ponto2D{" + "x=" + x + ", y=" + y + '}';
